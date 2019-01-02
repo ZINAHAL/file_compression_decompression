92e9e7c923823bb49dfe8f936334b4abd67a00fc
ZINAHAL <albaghdz@tcd.ie>
2019-01-02 19:19:01 +0000

    New changes are made to the initial header file as a result of implementing the insert and resize methods

--- priority_queue.h
+++ priority_queue.h
@@ -5,18 +5,21 @@
 typedef struct node{
     int key;
     char value;
-    struct node* left, right;
+    struct node *left, *right;    // always put the star with the name, not with the type
 } Node;
 
 typedef struct {
     int entered_nodes;
+    int capacity;
     Node* nodes;
 } PQ;
 
+// structs are either passed by referance or by value.
 
-void insert(PQ, Node);  // will insert a node into the PQ. That node could be a leaf node or the root of a tree
-Node delete_min(PQ);    // will delete and return the node with the lowest key
-void resize(PQ);
+void insert(PQ*, Node);  // will insert a node into the PQ. That node could be a leaf node or the root of a tree
+Node delete_min(PQ*);    // will delete and return the node with the lowest key
+void resize(PQ*, int);
+void exchange_nodes(Node*, int, int);
 
 
 #endif
