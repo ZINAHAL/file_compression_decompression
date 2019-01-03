d714b7ba3a3ff9f60dffb93305d380ead6ae1b9e
ZINAHAL <albaghdz@tcd.ie>
2019-01-03 20:44:18 +0000

    Changed the code from the idea of having an array of structs, by value, to an array of pointers each pointing to a struct. Added an extra function, is_less_than, to make the insert method less complex and easy to read

--- priority_queue.h
+++ priority_queue.h
@@ -11,15 +11,17 @@ typedef struct node{
 typedef struct {
     int entered_nodes;
     int capacity;
-    Node* nodes;
+    Node **nodes;
 } PQ;
 
 // structs are either passed by referance or by value.
 
-void insert(PQ*, Node);  // will insert a node into the PQ. That node could be a leaf node or the root of a tree
-Node delete_min(PQ*);    // will delete and return the node with the lowest key
-void resize(PQ*, int);
-void exchange_nodes(Node*, int, int);
+void insert(PQ*, Node*);  // will insert a node into the PQ. That node could be a leaf node or the root of a tree
+Node* delete_min(PQ*);    // will delete and return the node with the lowest key
+void resize(PQ*, int);   // if the PQ is completly full, then its size will be doubled, else if the PQ is quarterly full, then its size will be halved
+void exchange_nodes(Node**, int, int);
+int is_less_than(Node **, int, int);
+
 
 
 #endif
